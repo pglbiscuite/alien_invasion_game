91672b6259c65e459d89d2261a39257fecc6c498
pglbiscuite <114862250+pglbiscuite@users.noreply.github.com>
2023-12-16 09:49:39 +0200

    Refactoring code for Class AlienInvasion and added exception to the game instance.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from ship import  Ship
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
+
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
@@ -26,10 +27,13 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch Keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+
+    def _check_events(self):
+        """Respond to key presses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
@@ -38,9 +42,11 @@ class AlienInvasion:
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
+try:
+    if __name__ == '__main__':
+        # Make a game instance, and run the game.
 
-if __name__ == '__main__':
-    # Make a game instance, and run the game.
-
-    ai = AlienInvasion()
-    ai.run_game()
+        ai = AlienInvasion()
+        ai.run_game()
+except KeyboardInterrupt:
+    print('\nThank You for Choosing micro15 Games.')
